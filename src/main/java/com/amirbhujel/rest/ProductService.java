e103b4213386a1ffa50bf3e2d8de5f1da3b22eaa
gandakian <bhujel.amir@gmail.com>
2017-05-30 15:07:01 -0500

    Initialized a project to provide Spring Security

---
+++ src/main/java/com/amirbhujel/rest/ProductService.java
@@ -0,0 +1,17 @@
+package com.amirbhujel.rest;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import org.springframework.security.access.annotation.Secured;
+
+public interface ProductService {
+
+	
+	List<Product> getProducts();
+
+	int addProduct(Product product);
+}
